fb70aff89cbe4fe3c3f837be97d9f03c0b9aec1c
Harshit Sai Kannam <kannam.h@northeastern.edu>
2024-03-01 20:46:25 -0500

    ip

--- quiz/server.js
+++ quiz/server.js
@@ -8,27 +8,33 @@ const port = 8000;
 let users;
 fs.readFile(path.resolve(__dirname, '../data/users.json'), function(err, data) {
   console.log('reading file ... ');
-  if(err) throw err;
+  if(err) {
+    console.error('Failed to read file', err);
+    process.exit(1);
+  }
   users = JSON.parse(data);
 })
 
-const addMsgToRequest = function (req, res, next) {
+const addUserToRequest = function (req, res, next) {
   if(users) {
     req.users = users;
     next();
   }
   else {
-    return res.json({
+    return res.status(404).json({
         error: {message: 'users not found', status: 404}
     });
   }
   
 }
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(
   cors({origin: 'http://localhost:3000'})
 );
-app.use('/read/usernames', addMsgToRequest);
+app.use('/read/usernames', addUserToRequest);
 
 app.get('/read/usernames', (req, res) => {
   let usernames = req.users.map(function(user) {
@@ -37,18 +43,19 @@ app.get('/read/usernames', (req, res) => {
   res.send(usernames);
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/write/adduser', addMsgToRequest);
+app.use('/write/adduser', addUserToRequest);
 
 app.post('/write/adduser', (req, res) => {
   let newuser = req.body;
   req.users.push(newuser);
   fs.writeFile(path.resolve(__dirname, '../data/users.json'), JSON.stringify(req.users), (err) => {
-    if (err) console.log('Failed to write');
-    else console.log('User Saved');
+    if (err) {
+      console.error('Failed to write', err);
+      return res.status(500).send('Failed to write user data');
+    }
+    console.log('User Saved');
+    res.send('done');
   });
-  res.send('done');
 })
 
 app.listen(port, () => {
