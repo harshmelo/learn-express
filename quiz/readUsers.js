59c889d69508ca8b6b543d0a29683d232c7c8b0d
Harshit Sai Kannam <kannam.h@northeastern.edu>
2024-03-12 16:08:54 -0400

    ip activity

--- quiz/readUsers.js
+++ quiz/readUsers.js
@@ -1,2 +1,15 @@
 const express = require('express')
+const fs = require('fs')
 const router = express.Router();
+
+router.get('/users', (req, res) => {
+  fs.readFile('users.json', (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading users data');
+      return;
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+module.exports = router;
